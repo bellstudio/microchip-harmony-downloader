2c34141fe407d79a1ef6511225b3c2535f99b77c
bellstudio <bellstudio@126.com>
2023-07-16 00:55:34 +0800

    clean/sync code

--- gitee/decode_catalog.py
+++ gitee/decode_catalog.py
@@ -15,49 +15,8 @@ repo_property = []
 repo_list = []
 repo_count=0
 
-# filename = os.listdir(root_path)
-# print(filename)
-def list_all_ymls(base_path):
-    fileNames = next(os.walk(base_path))[2]
-    # repo_list.clear()
-    repo_count = 0;
-    for files in fileNames:
-        if files.endswith('.yml'):
-            filepath = base_path + '/' + files
-            with open(filepath, 'r') as file:
-                prime_service = yaml.safe_load(file)
-                for item in prime_service['catalog']['host']['packages']:
-                    # repo_property.clear()
-                    # print(files)
-                    # print(item["name"])
-                    if 'type' in item:
-                        repo_property.append(item['name'])
-                        repo_property.append(item['type'])
-                        repo_property.append(
-                            'https://' + prime_service['catalog']['host']['url'] + prime_service['catalog']['host'][
-                                'path'] + item['name'] + '.git')
-                        # repo_list[repo_count].append(item['name'])
-                        # repo_list[repo_count+1].append(item['type'])
-                        # repo_list[repo_count+2].append(
-                        #     'https://' + prime_service['catalog']['host']['url'] + prime_service['catalog']['host'][
-                        #         'path'] + item['name'] + '.git')
-                        repo_count = repo_count + 3
-                        # print(repo_property)
-                        # print(item['type']+': ' + item['name'])
-                        # print('https://' + prime_service['catalog']['host']['url'] + prime_service['catalog']['host']['path'] + item['name'] + '.git')
-                    else:
-                        repo_property.append(item['name'])
-                        repo_property.append('NONE')
-                        repo_property.append(
-                            'https://' + prime_service['catalog']['host']['url'] + prime_service['catalog']['host'][
-                                'path'] + item['name'] + '.git')
-                        # repo_list[repo_count].append(repo_property)
-                        repo_count = repo_count + 1
-                        # print(repo_property)
-
-    # display how many repositories we have
-    # print(len(repo_list))
-
+# print(prime_service)
+# print(prime_service['catalog']['host']['packages'][1]['name'])
 def list_all_ymls_old(base_path):
     fileNames = next(os.walk(base_path))[2]
     for files in fileNames:
@@ -95,38 +54,12 @@ def list_all_ymls_old(base_path):
         #       package-check: true
         #       third_party: false
 
-#
-# print(len(name_list))
-# print(len(url_list))
-# print(len(type_list))
 
-# count = 0
-# loop = len(name_list)
-# for item in range(loop):
-#     if type_list[item] == 'api':
-#         print(type_list[item] + ': ' + name_list[item])
-#         print(url_list[item])
-#         count = count + 1
-
-# def sort_repo_by_type():
-    # repo_list.sort(key=(lambda x: x[1]))
-
-
-
-# print(prime_service)
-# print(prime_service['catalog']['host']['packages'][1]['name'])
 def main(argv):
     count = 0
     func_sel = ""
     repo_type = ""
-    """
-     通过sys模块来识别参数demo, http://blog.csdn.net/ouyang_peng/
-    """
-    # print('参数个数为:', len(sys.argv), '个参数。')
-    # print('参数列表:', str(sys.argv))
-    # print('脚本名为：', sys.argv[0])
-    # for i in range(1, len(sys.argv)):
-    #     print('参数 %s 为：%s' % (i, sys.argv[i]))
+    
     try:
         opts, args = getopt.getopt(sys.argv[1:], "hf:t:", ["help", "function=", "type="])
     except getopt.GetoptError:
@@ -144,20 +77,6 @@ def main(argv):
             repo_type = arg
         elif opt in ("-f", "--func"):
             func_sel = arg
-
-    # print('func_sel：', func_sel)
-    # print('repo_type：', repo_type)
-
-    # list_all_ymls(root_path)
-    # print('Total repositories: ' + str(len(repo_property)))
-    # # sort_repo_by_type()
-    # repo_list = [[] for i in range(len(repo_property))]
-    # print(repo_property)
-    # for item in range(len(repo_property)):
-    #     repo_list[item].append(repo_property[item])
-    #
-    # print(repo_list)
-    # sys.exit()
 
     list_all_ymls_old(root_path)
     
@@ -190,49 +109,6 @@ def main(argv):
                 print(type_list[item] + ': ' + name_list[item])
                 print(url_list[item])
                 count = count + 1
-
-
-    #
-    # list_all_ymls(root_path)
-    #
-    #
-    # loop = len(name_list)
-    #
-    # if len(sys.argv) == 2:    # print as required
-    #     if (sys.argv[1] == 'api') or (sys.argv[1] == 'application') or (sys.argv[1] == 'device') or (sys.argv[1] == 'external'):
-    #         for item in range(loop):
-    #             if type_list[item] == sys.argv[1]:
-    #                 print(type_list[item] + ': ' + name_list[item])
-    #                 print(url_list[item])
-    #                 count = count + 1
-    #
-    #     elif sys.argv[1] == 'name':
-    #         for item in range(loop):
-    #             print(name_list[item])
-    #     else:    # print all
-    #         print('Incorrect parameter, list all items.')
-    #         for item in range(loop):
-    #             print(type_list[item] + ': ' + name_list[item])
-    #             print(url_list[item])
-    #             count = count + 1
-    #
-    #     print('Total Repo: ' + str(count))
-    # elif len(sys.argv) == 3:    # print as required
-    #     if (sys.argv[1] == 'api') or (sys.argv[1] == 'application') or (sys.argv[1] == 'device') or (sys.argv[1] == 'external'):
-    #         for item in range(loop):
-    #             if type_list[item] == sys.argv[1]:
-    #                 print(type_list[item] + ': ' + name_list[item])
-    #                 print(url_list[item])
-    #                 count = count + 1
-    #
-    #     elif sys.argv[1] == 'name':
-    #         for item in range(loop):
-    #             print(name_list[item])
-    # else:
-    #     print('Please specify api/application/device/external/all for range.')
-    #     print('eg. Use below command to list all api repositories:')
-    #     print('decode_catalog.py [name] [api] []')
-    #     print('     decode_catalog.py api')
 
 
 if __name__ == "__main__":
--- gitee/h3_create_new_config_gitee.bat
+++ gitee/h3_create_new_config_gitee.bat
@@ -1,28 +1,66 @@
 
-@echo **************************************************
-@echo *   This will REMOVE all existing config files   *
-@echo *  ALL features are turned off after generating  *
-@echo * Please manual edit config file after generated *
-@echo **************************************************
-@echo OFF
-pause
-
-
+SETLOCAL
+rem Please modify this according to your own system setting
 set  gitpath="C:\Git\bin\git.exe"
 
+
+@echo OFF
+rem DO NOT modify other contents
+
+if "%~1"=="/h" (
+	@echo.This is a batch file create files according to catalog file configuration
+	@echo.User should modify configuration files after creating
+	@echo.All modules are disabled by default
+	@echo.
+	@echo.Usage:
+	@echo.h3_create_new_config_gitee.bat [/h] [-h] [/update]
+	@echo. /h, -h	    Show help information
+	@echo. /update 	    Try update local catalog files even existed
+	@echo.
+	@echo. -- eg. C:\harmony\h3_create_new_config_gitee.bat /h
+	@echo.    This will display this help screen
+	goto end
+	)
+if "%~1"=="-h" (
+	@echo.This is a batch file create files according to catalog file configuration
+	@echo.User should modify configuration files after creating
+	@echo.All modules are disabled by default
+	@echo.
+	@echo.Usage:
+	@echo.h3_create_new_config_gitee.bat [/h] [-h] [/update]
+	@echo. /h, -h	Show help information
+	@echo. /update 	    Try update local catalog files even existed
+	@echo.
+	@echo. -- eg. C:\harmony\h3_create_new_config_gitee.bat /h
+	@echo.    This will display this help screen
+	goto end
+	)
+
+@echo.	
+@echo.      *------------------------------------------------*
+@echo.      *   This will REMOVE all existing config files   *
+@echo.      *  ALL features are turned off after generating  *
+@echo.      * Please manual edit config file after generated *
+@echo.      *------------------------------------------------*
+@echo.
+@echo OFF
+pause	
+
 IF EXIST catalog (
-	@echo update catalog files
-	cd .\catalog
-	%gitpath% fetch
-	%gitpath% checkout .
-	%gitpath% checkout master
-	cd ..
+	if "%~1"=="/update" (
+		@echo update catalog files
+		cd .\catalog
+		%gitpath% fetch
+		%gitpath% checkout .
+		%gitpath% checkout master
+		cd ..
+		)
 	) ELSE (
 	@echo No existing catalog file
 	@echo Clone catalog file from github
 	%gitpath% clone https://gitee.com/Microchip-MPLAB-Harmony/catalog.git
 	)
-pause
+
 @echo.Remove old configuration files...
 del /Q h3gitee*.ini
 @echo. Searching Device...
@@ -37,3 +75,6 @@ python decode_catalog.py -f configfile -t application >> h3gitee_app_config.ini
 python decode_catalog.py -f configfile -t external >> h3gitee_external_config.ini
 
 @echo New config files generated with success!
+
+:end
+ENDLOCAL
